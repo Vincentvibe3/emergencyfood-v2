b8effb6f0a6a3fd8d5ef06f6e0c9b271ea244eb8
Vincent <vincentvibe3@gmail.com>
2022-03-13 20:43:47 -0400

    Improved command update flow

--- src/main/kotlin/io/github/vincentvibe3/emergencyfood/internals/CommandManager.kt
+++ src/main/kotlin/io/github/vincentvibe3/emergencyfood/internals/CommandManager.kt
@@ -10,6 +10,11 @@ import io.github.vincentvibe3.emergencyfood.core.Bot
 import io.github.vincentvibe3.emergencyfood.internals.ConfigLoader.Channel
 import io.github.vincentvibe3.emergencyfood.utils.Logging
 import net.dv8tion.jda.api.interactions.commands.Command
+<<<<<<< HEAD
+=======
+import java.util.concurrent.ExecutionException
+import kotlin.reflect.full.createInstance
+>>>>>>> 3888cf6 (Improved command update flow)
 
 object CommandManager {
 
@@ -62,13 +67,13 @@ object CommandManager {
         }
     }
 
-    /*this method is only currently used to accelerate testing and
+    /* this method is only currently used to accelerate testing and
     * therefore does not support use of actual guild commands*/
     fun registerGuildRemote(channel: Channel) {
         Logging.logger.info("Starting guild command registration")
-
         //register guild commands if not on stable
         if (channel != Channel.STABLE) {
+<<<<<<< HEAD
             slashCommandsList.forEach {
                 val command = it.value as GenericCommand
                 if (command is SlashCommand){
@@ -80,34 +85,81 @@ object CommandManager {
                                 { Logging.logger.error("Failed to add ${command.name} to ${guild.name}") }
                             )
                         }
+=======
+            val toDelete = ArrayList<String>()
+            val client = Bot.getClientInstance()
+            val guild = client.guilds.first { it.id == Config.testServer }
+            val updates = guild
+                .updateCommands()
+            var remoteCommands: MutableList<Command> = ArrayList()
+            try {
+                remoteCommands = guild.retrieveCommands().complete()
+                val names = remoteCommands.map { remote -> remote.name }
+                val localNames = slashCommandsList.map {local -> (local.value as GenericCommand).name}
+                names.filter { remote -> localNames.contains(remote).not() }
+                    .forEach { name ->
+                        println(name)
+                        toDelete.add(name)
+>>>>>>> 3888cf6 (Improved command update flow)
                     }
-                }
+            } catch (e:ExecutionException){
+                Logging.logger.warn("Canceled guild commands fetch")
+            } catch (e:RuntimeException){
+                Logging.logger.warn("Failed to fetch remote guild commands. Commands will be updated without deletion message")
             }
-        }
-        //remove old guild commands
-        Bot.getClientInstance().guilds.forEach { guild ->
-            guild.retrieveCommands().queue({ commandList ->
-                commandList.filter { !slashCommandsList.containsKey(it.name) }.forEach { command ->
-                    try {
-                        command.delete().queue(
-                            { Logging.logger.info("Deleted ${command.name} from ${guild.name}") },
-                            { Logging.logger.error("Failed to delete ${command.name} from ${guild.name}") }
-                        )
-                    } catch (e: IllegalAccessException) {
-                        Logging.logger.error("Failed to delete ${command.name} ${guild.name}")
+
+
+            slashCommandsList.forEach {
+                val command = it.value
+                val commandData = command.command
+                updates.addCommands(commandData)
+            }
+            updates.queue ({
+                var updateCount = 0
+                it.forEach { addedCommand ->
+                    if (remoteCommands.contains(addedCommand).not()) {
+                        Logging.logger.info("Added ${addedCommand.name} to ${guild.name}")
+                    } else {
+                        updateCount++
                     }
                 }
+                Logging.logger.info("Updated $updateCount commands in ${guild.name}")
             }, {
-                Logging.logger.error("Failed to fetch commands for ${guild.name} ")
+                Logging.logger.error("Failed to update commands in ${guild.name}")
+                it.printStackTrace()
             })
+            toDelete.forEach {
+                Logging.logger.warn("$it will be deleted in ${guild.name}")
+            }
         }
     }
 
     //register commands on discord
     fun registerRemote(channel: Channel) {
         Logging.logger.info("Starting command registration")
+        val toDelete = ArrayList<String>()
+        val client = Bot.getClientInstance()
+        val updates = client
+            .updateCommands()
+        var remoteCommands: MutableList<Command> = ArrayList()
+        try {
+            remoteCommands = client.retrieveCommands().complete()
+            val names = remoteCommands.map { remote -> remote.name }
+            val localNames = slashCommandsList.map {local -> (local.value as GenericCommand).name}
+            names.filter { remote -> localNames.contains(remote).not() }
+                .forEach { name ->
+                    println(name)
+                    toDelete.add(name)
+                }
+            remoteCommands.first()
+        } catch (e:ExecutionException){
+            Logging.logger.warn("Canceled commands fetch")
+        } catch (e:RuntimeException){
+            Logging.logger.warn("Failed to fetch remote commands. Commands will be updated without deletion message")
+        }
         //upsert new commands
         slashCommandsList.forEach {
+<<<<<<< HEAD
             val command = it.value as GenericCommand
             val isStable = !command.beta
             if (command is SlashCommand){
@@ -137,9 +189,22 @@ object CommandManager {
             Bot.getClientInstance().guilds.forEach { guild ->
                 guild.retrieveCommands().queue { guildCommands ->
                     guildCommands.forEach { toDelete["guild"]?.add(it) }
+=======
+            val command = it.value
+            val name = (command as GenericCommand).name
+            val isStable =
+                command::class.annotations.none { annotation -> annotation.annotationClass == Bot.Beta::class.createInstance().annotationClass }
+            val commandData = command.command
+            if (channel == Channel.STABLE && !isStable) {
+                if (toDelete.contains(name).not()){
+                    toDelete.add(name)
+>>>>>>> 3888cf6 (Improved command update flow)
                 }
+            } else {
+                updates.addCommands(commandData)
             }
         }
+<<<<<<< HEAD
 
         Bot.getClientInstance().retrieveCommands().queue { remoteCommandList ->
             //delete remote commands that are locally marked as beta
@@ -168,9 +233,25 @@ object CommandManager {
                     } catch (e: IllegalAccessException) {
                         Logging.logger.error("Failed to delete ${command.name} ${type.key}")
                     }
+=======
+        toDelete.forEach {
+            Logging.logger.warn("$it will be deleted")
+        }
+        updates.queue({
+            var updateCount = 0
+            it.forEach { addedCommand ->
+                if (remoteCommands.contains(addedCommand).not()) {
+                    Logging.logger.info("Added ${addedCommand.name}")
+                } else {
+                    updateCount++
+>>>>>>> 3888cf6 (Improved command update flow)
                 }
             }
-        }
+            Logging.logger.info("Updated $updateCount commands")
+        }, {
+            Logging.logger.error("Failed to update commands")
+            it.printStackTrace()
+        })
     }
 
 }
