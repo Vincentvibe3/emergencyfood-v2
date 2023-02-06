55e3d01ff8c4cb26d10f3da48bd7ba94adcdf213
Vincent <vincentvibe3@gmail.com>
2023-02-05 19:25:46 -0500

    Update ef-player

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,7 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0-RC")
     implementation("ch.qos.logback:logback-classic:1.4.5")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
-    implementation("com.github.Vincentvibe3:ef-player:v.1.3.11")
+    implementation("com.github.Vincentvibe3:ef-player:v.1.3.12")
     testImplementation(kotlin("test"))
 }
 
