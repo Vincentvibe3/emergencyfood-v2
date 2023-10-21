758f57ef3e2d1e96ecffcd7af3cfd2325c297588
Vincent <vincentvibe3@gmail.com>
2023-10-20 21:40:19 -0400

    Update ef-player and JDA

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 group = "io.github.vincent.emergencyfood"
-version = "1.7.31"
+version = "1.7.32"
 
 repositories {
     mavenCentral()
@@ -19,12 +19,12 @@ repositories {
 }
 
 dependencies {
-    implementation("net.dv8tion:JDA:5.0.0-beta.13")
+    implementation("net.dv8tion:JDA:5.0.0-beta.15")
 //    implementation ("com.github.walkyst:lavaplayer-fork:1.3.99.2")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0-RC")
     implementation("ch.qos.logback:logback-classic:1.4.5")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
-    implementation("com.github.Vincentvibe3:ef-player:v.1.4.2")
+    implementation("com.github.Vincentvibe3:ef-player:v.1.4.3")
     testImplementation(kotlin("test"))
 }
 
