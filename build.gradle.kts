056f11b3afda12556145110dfe5a20fe94862bb7
Vincent <vincentvibe3@gmail.com>
2024-08-10 21:05:11 -0400

    update ef-player

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 group = "io.github.vincent.emergencyfood"
-version = "1.8.7b"
+version = "1.8.8"
 
 repositories {
     mavenCentral()
@@ -24,7 +24,7 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0-RC")
     implementation("ch.qos.logback:logback-classic:1.5.6")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
-    implementation("com.github.Vincentvibe3:ef-player:v.1.4.8")
+    implementation("com.github.Vincentvibe3:ef-player:v.1.4.9")
     testImplementation(kotlin("test"))
 }
 
